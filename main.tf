e02da91aa06a6635c1b5b3032ae17f46d20601c2
John Thompson <johnthompson@johnthomp-ltm.internal.salesforce.com>
2020-11-25 11:24:17 -1000

    Added Web NSG

--- main.tf
+++ main.tf
@@ -70,6 +70,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "98.155.209.54"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "web"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
