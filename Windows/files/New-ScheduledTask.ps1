4a6e582c7ac95bf01b9c1fc38260993a57c874e4
johnthompson365 <john@thompson365.com>
2021-03-14 16:56:15 -0700

    Updated sch task

--- Windows/files/New-ScheduledTask.ps1
+++ Windows/files/New-ScheduledTask.ps1
@@ -4,7 +4,7 @@
 $a = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -ExecutionPolicy Bypass "c:\jt365\wintab-deploy-original.ps1"'    
 $p = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest #-GroupId "BUILTIN\Administrators"
 $t = New-ScheduledTaskTrigger -AtLogon
-$s = New-ScheduledTaskSettingsSet -AsJob
+$s = New-ScheduledTaskSettingsSet
 $Task = New-ScheduledTask -Action $a -Principal $p -Trigger $t -Settings $s
 Register-ScheduledTask -TaskName "Tableau Installer" -InputObject $Task 
 #Register-ScheduledTask -Action $a -Trigger $t -principal $p -TaskName "Tableau Installer" -Description "Task running the Tableau powershell install script on logon" -AsJob -Force
