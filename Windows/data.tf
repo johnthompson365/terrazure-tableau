3220b71b4019c7b28ccf29d81fede7ff2d3d8928
johnthompson365 <john@thompson365.com>
2021-04-30 20:06:30 -0700

    Updated for testing

--- Windows/data.tf
+++ Windows/data.tf
@@ -1,4 +1,6 @@
-data "azurerm_public_ip" "ip" {
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/public_ip
+
+ data "azurerm_public_ip" "ip" {
   name                = azurerm_public_ip.publicip.name
   resource_group_name = azurerm_windows_virtual_machine.windows_vm.resource_group_name
   depends_on          = [azurerm_windows_virtual_machine.windows_vm]
--- Windows/outputs.tf
+++ Windows/outputs.tf
@@ -2,20 +2,20 @@ output "public_ip_address" {
   value = data.azurerm_public_ip.ip.ip_address
 }
 
-# output "vm_id" {
-#   description = "Id of the Virtual machine"
-#   value       = azurerm_windows_virtual_machine.vm.id
-# }
+output "vm_id" {
+  description = "Id of the Virtual machine"
+  value       = azurerm_windows_virtual_machine.vm.id
+}
 
-# output "vm_name" {
-#   description = "Name of the Virtual machine"
-#   value       = azurerm_windows_virtual_machine.vm.name
-# }
+output "vm_name" {
+  description = "Name of the Virtual machine"
+  value       = azurerm_windows_virtual_machine.vm.name
+}
 
-# output "vm_public_ip_address" {
-#   description = "Public IP address of the Virtual machine"
-#   value       = var.public_ip_sku == null ? null : join("", azurerm_public_ip.public_ip.*.ip_address)
-# }
+output "vm_public_ip_address" {
+  description = "Public IP address of the Virtual machine"
+  value       = var.public_ip_sku == null ? null : join("", azurerm_public_ip.public_ip.*.ip_address)
+}
 
 
 # output "vm_public_domain_name_label" {
@@ -23,10 +23,10 @@ output "public_ip_address" {
 #   value       = var.public_ip_sku == null ? null : join("", azurerm_public_ip.public_ip.*.domain_name_label)
 # }
 
-# output "vm_private_ip_address" {
-#   description = "Private IP address of the Virtual machine"
-#   value       = azurerm_network_interface.nic.private_ip_address
-# }
+output "vm_private_ip_address" {
+  description = "Private IP address of the Virtual machine"
+  value       = azurerm_network_interface.nic.private_ip_address
+}
 
 # output "vm_winrm_certificate_key_vault_id" {
 #   description = "Id of the generated certificate in the input Key Vault"
@@ -43,17 +43,17 @@ output "public_ip_address" {
 #   value       = azurerm_key_vault_certificate.winrm_certificate.thumbprint
 # }
 
-# output "vm_nic_name" {
-#   description = "Name of the Network Interface Configuration attached to the Virtual Machine"
-#   value       = azurerm_network_interface.nic.name
-# }
+output "vm_nic_name" {
+  description = "Name of the Network Interface Configuration attached to the Virtual Machine"
+  value       = azurerm_network_interface.nic.name
+}
 
-# output "vm_nic_id" {
-#   description = "ID of the Network Interface Configuration attached to the Virtual Machine"
-#   value       = azurerm_network_interface.nic.id
-# }
+output "vm_nic_id" {
+  description = "ID of the Network Interface Configuration attached to the Virtual Machine"
+  value       = azurerm_network_interface.nic.id
+}
 
-# output "vm_nic_ip_configuration_name" {
-#   description = "Name of the IP Configuration for the Network Interface Configuration attached to the Virtual Machine"
-#   value       = local.ip_configuration_name
-# }
+output "vm_nic_ip_configuration_name" {
+  description = "Name of the IP Configuration for the Network Interface Configuration attached to the Virtual Machine"
+  value       = local.ip_configuration_name
+}
